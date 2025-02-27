a87f246445f49f58d5c159e7a03c06056020b4d7
Törcsi <torcsiattila93@gmail.com>
2025-02-27 19:38:51 +0100

    feat: initialize database in app module

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -4,10 +4,15 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ExerciseModule } from './modules/exercises/exercises.module';
 import { UsersModule } from './modules/users/users.module';
 import { ExerciseTypeModule } from './modules/exercise-types/exercise-types.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: getDatabaseConfig,
+    }),
     AuthModule,
     UsersModule,
     ExerciseModule,
